d08ff06199071591d0e9129001014e243465a906
Deepak Gupta <deepakkumarg968@gmail.com>
2021-03-28 16:43:04 +0530

    First Commit

---
+++ Java/src/arrays/ArrayIntro.java
@@ -0,0 +1,24 @@
+package arrays;
+
+import java.util.*;
+
+public class ArrayIntro {
+
+	public static void main(String[] args) {
+		//int[] marks = new int[5];
+		
+		//int[] marks;
+		//marks = new int[5];
+		
+		int marks[] = new int[5];
+		
+		Scanner sc = new Scanner(System.in);
+		
+		for(int i =0;i<5;i++)
+		{
+			System.out.println(marks[i]);
+		}
+
+	}
+
+}
